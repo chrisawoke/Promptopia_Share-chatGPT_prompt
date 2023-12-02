09056c9e9cb0798cf015ec7ddb4afa1ee9b11003
Chris Awoke <uchechristian27@gmail.com>
2023-12-02 01:24:05 +0100

    Made a few improvements

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+"use client";
 
 import { SessionProvider } from "next-auth/react";
 
 const Provider = ({ children, session }) => {
-  return <div>Provider</div>;
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 };
 
 export default Provider;
